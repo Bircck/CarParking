f5e926f6bbab59f552077c5a864ee39bb661b499
Bircck <s195458@student.dtu.dk>
2021-02-22 13:27:22 +0100

    - parkerings ide - data ide - generelle ting sat op

---
+++ app/src/main/java/com/example/carparking/CarStatistics.java
@@ -0,0 +1,107 @@
+package com.example.carparking;
+
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sdsmdg.harjot.vectormaster.VectorMasterView;
+import com.sdsmdg.harjot.vectormaster.models.PathModel;
+
+public class CarStatistics extends AppCompatActivity implements View.OnClickListener{
+
+    /*
+    videos on audio if/when needed:
+    https://www.youtube.com/watch?v=C_Ka7cKwXW0&list=PLrnPJCHvNZuCTyi1Gesn5TTbKVuhAjf8q&ab_channel=CodinginFlow
+
+    github bibliotekt til battery cirklen/vektor ting
+    https://github.com/harjot-oberai/VectorMaster
+
+    mulight vektor alternativ
+    https://github.com/tarek360/RichPath
+
+    generelt android vector fis
+    https://developer.android.com/reference/android/graphics/drawable/AnimatedVectorDrawable
+     */
+
+    Button change_battery_button;
+
+    VectorMasterView heartVector;
+
+    // find the correct path using name
+    PathModel outline;
+    PathModel innerline;
+
+    Float battery = 0.65f;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_statistics);
+
+        heartVector = (VectorMasterView) findViewById(R.id.circle_vector_1);
+
+        // find the correct path using name
+        outline = heartVector.getPathModelByName("outline");
+        innerline = heartVector.getPathModelByName("innerline");
+
+        // set the stroke color
+        outline.setStrokeColor(Color.parseColor("#ED4337"));
+        innerline.setStrokeColor(Color.parseColor("#000000"));
+
+        // set trim path start (values are given in fraction of length)
+        //outline.setTrimPathStart(0.1f);
+
+        // set trim path end (values are given in fraction of length)
+        //outline.setTrimPathEnd(0.65f);
+
+        // set the fill color (if fill color is not set or is TRANSPARENT, then no fill is drawn)
+
+        //GradientDrawable gradientDrawable = new GradientDrawable(
+        //GradientDrawable.Orientation.TOP_BOTTOM, colors);
+        change_battery_button = findViewById(R.id.change_battery_button);
+        change_battery_button.setOnClickListener(this);
+
+        heartVector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // initialise valueAnimator and pass start and end float values
+                ValueAnimator valueAnimator = ValueAnimator.ofFloat(0.1f, 0.4f);
+                valueAnimator.setDuration(0);
+
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+                        // set trim end value and update view
+                        outline.setTrimPathEnd((Float) valueAnimator.getAnimatedValue());
+                        heartVector.update();
+                    }
+                });
+                valueAnimator.start();
+            }
+        });
+
+    }
+
+    public void onClick(View v) {
+        if (v == change_battery_button) {
+
+            battery += 0.05f;
+
+            if(battery > 0.9){
+                battery = 0.05f;
+            }
+
+            outline.setTrimPathEnd(battery);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/carparking/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.carparking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button testing_button, car_statistics_screen_button;
+    ImageView testing_image;
+    ImageView first_bar, second_bar, third_bar;
+
+    int counter;
+    int green = Color.parseColor("#058a00");
+    int yellow = Color.parseColor("#d4d400");
+    int red = Color.parseColor("#9c1000");
+    int black = Color.parseColor("#000000");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        car_statistics_screen_button = findViewById(R.id.car_statistics_screen_button);
+        car_statistics_screen_button.setOnClickListener(this);
+        testing_button = findViewById(R.id.testing_button);
+        testing_image = findViewById(R.id.testing_image);
+
+        first_bar = findViewById(R.id.first_bar);
+        second_bar = findViewById(R.id.second_bar);
+        third_bar = findViewById(R.id.third_bar);
+
+        counter = 0;
+
+        testing_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                counter++;
+
+                if(counter == 1){
+                    first_bar.setColorFilter(green);
+                } else if(counter == 2){
+                    second_bar.setColorFilter(yellow);
+                } else if(counter == 3){
+                    third_bar.setColorFilter(red);
+                } else{
+                    counter = 0;
+                    first_bar.setColorFilter(black);
+                    second_bar.setColorFilter(black);
+                    third_bar.setColorFilter(black);
+                }
+
+                System.out.println("asdf");
+                //testing_image.setColorFilter(green);
+                //testing_image.setColorFilter(getApplicationContext().getResources().getColor(R.color.purple_200));
+            }
+        });
+    }
+
+    public void onClick(View v) {
+        if (v == car_statistics_screen_button) {
+            Intent i = new Intent(this, CarStatistics.class);
+            startActivity(i);
+        }
+    }
+}
