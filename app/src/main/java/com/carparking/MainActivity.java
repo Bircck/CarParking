6d9e5b319107676282766d50d1b49c11982af730
Bircck <s195458@student.dtu.dk>
2021-03-13 20:35:45 +0100

    Opdateret UI på parking side - reverse+parking symboler - parking distance vector image - animationer / opdatering af distance vector image - generelt farve styling og opdatering af tema - floatingactionbutton navigation til statistik side

--- app/src/main/java/com/carparking/MainActivity.java
+++ app/src/main/java/com/carparking/MainActivity.java
@@ -1,71 +1,201 @@
 package com.carparking;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.sdsmdg.harjot.vectormaster.VectorMasterView;
+import com.sdsmdg.harjot.vectormaster.models.PathModel;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button testing_button, car_statistics_screen_button;
-    ImageView testing_image;
-    ImageView first_bar, second_bar, third_bar;
+    FloatingActionButton car_statistics_screen_button;
 
-    int counter;
-    int green = Color.parseColor("#058a00");
-    int yellow = Color.parseColor("#d4d400");
-    int red = Color.parseColor("#9c1000");
-    int black = Color.parseColor("#000000");
+    ImageView reverse_symbol, parking_symbol;
+
+    VectorMasterView distance_vector;
+
+    // find the correct path using name
+    PathModel distance1;
+    PathModel distance2;
+    PathModel distance3;
+    PathModel distance4;
+    int mode = 0;
+
+    //Thread animationThread;
+
+    // initialize valueAnimator and pass start and end color values
+    ValueAnimator valueAnimator;
+    Resources res;
+
+    int color_parked;
+    int color_reverse;
+    int color_notActive;
+    int parked_active = 0;
+    int reverse_active = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //park/revers symbols
+        parking_symbol = findViewById(R.id.parking_symbol);
+        parking_symbol.setOnClickListener(this);
+        reverse_symbol = findViewById(R.id.reverse_symbol);
+        reverse_symbol.setOnClickListener(this);
+        declareColors();
+
+        distance_vector = (VectorMasterView) findViewById(R.id.distance_vector);
+        distance_vector.setOnClickListener(this);
+
+        // find the correct path using name
+        distance1 = distance_vector.getPathModelByName("distance1");
+        distance2 = distance_vector.getPathModelByName("distance2");
+        distance3 = distance_vector.getPathModelByName("distance3");
+        distance4 = distance_vector.getPathModelByName("distance4");
+
+        //other stuff
         car_statistics_screen_button = findViewById(R.id.car_statistics_screen_button);
         car_statistics_screen_button.setOnClickListener(this);
-        testing_button = findViewById(R.id.testing_button);
-        testing_image = findViewById(R.id.testing_image);
-
-        first_bar = findViewById(R.id.first_bar);
-        second_bar = findViewById(R.id.second_bar);
-        third_bar = findViewById(R.id.third_bar);
-
-        counter = 0;
-
-        testing_button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                counter++;
-
-                if(counter == 1){
-                    first_bar.setColorFilter(green);
-                } else if(counter == 2){
-                    second_bar.setColorFilter(yellow);
-                } else if(counter == 3){
-                    third_bar.setColorFilter(red);
-                } else{
-                    counter = 0;
-                    first_bar.setColorFilter(black);
-                    second_bar.setColorFilter(black);
-                    third_bar.setColorFilter(black);
-                }
-
-                System.out.println("asdf");
-                //testing_image.setColorFilter(green);
-                //testing_image.setColorFilter(getApplicationContext().getResources().getColor(R.color.purple_200));
-            }
-        });
     }
 
     public void onClick(View v) {
         if (v == car_statistics_screen_button) {
             Intent i = new Intent(this, CarStatistics.class);
             startActivity(i);
+        }
+        if (v == distance_vector) {
+            distanceVectorUpdater();
+        }
+        if (v == parking_symbol){
+            updateRevParkSymbols(v);
+        }
+        if (v == reverse_symbol){
+            updateRevParkSymbols(v);
+        }
+    }
+
+    public void declareColors(){
+        //colors on buttons
+        final Context context=this;
+        res = context.getResources();
+        color_parked = res.getColor(R.color.red_900);
+        color_reverse = getResources().getColor(R.color.white);
+        color_notActive = getResources().getColor(R.color.main_lightgrey);
+        parking_symbol.setColorFilter(color_notActive, PorterDuff.Mode.SRC_ATOP);
+        reverse_symbol.setColorFilter(color_notActive, PorterDuff.Mode.SRC_ATOP);
+    }
+
+    public void updateRevParkSymbols(View clicked_symbol){
+        //parked
+        if(clicked_symbol == parking_symbol){
+            if(parked_active == 0){
+                parked_active++;
+                parking_symbol.setColorFilter(color_parked, PorterDuff.Mode.SRC_ATOP);
+            } else if(parked_active >= 1){
+                parked_active = 0;
+                parking_symbol.setColorFilter(color_notActive, PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+        //reverse
+        if(clicked_symbol == reverse_symbol){
+            if(reverse_active == 0){
+                reverse_active++;
+                reverse_symbol.setColorFilter(color_reverse, PorterDuff.Mode.SRC_ATOP);
+            } else if(reverse_active >= 1){
+                reverse_active = 0;
+                reverse_symbol.setColorFilter(color_notActive, PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+    }
+
+    public void distanceVectorUpdater(){
+        mode++;
+        if(mode == 1){
+            //repeating animation
+            valueAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), Color.parseColor("#111111"), Color.parseColor("#555555"));
+            valueAnimator.setDuration(2000);
+            valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+            System.out.println(valueAnimator.getRepeatCount());
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    // set fill color and update view
+                    distance1.setFillColor((Integer) valueAnimator.getAnimatedValue());
+                    distance2.setFillColor((Integer) valueAnimator.getAnimatedValue());
+                    distance3.setFillColor((Integer) valueAnimator.getAnimatedValue());
+                    distance4.setFillColor((Integer) valueAnimator.getAnimatedValue());
+
+                    distance_vector.update();
+                }
+            });
+            valueAnimator.start();
+        }
+        else if(mode == 2){
+            valueAnimator.cancel();
+
+            // set the fill color (if fill color is not set or is TRANSPARENT, then no fill is drawn)
+            distance1.setFillColor(Color.parseColor("#555555"));
+            distance2.setFillColor(Color.parseColor("#555555"));
+            distance3.setFillColor(Color.parseColor("#555555"));
+            distance4.setFillColor(Color.parseColor("#555555"));
+
+            distance_vector.update();
+        }
+        else if(mode == 3){
+            // set the fill color (if fill color is not set or is TRANSPARENT, then no fill is drawn)
+            distance4.setFillAlpha(0);
+            distance3.setFillColor(Color.parseColor("#4f3d3d"));
+            distance2.setFillColor(Color.parseColor("#4f3d3d"));
+            distance1.setFillColor(Color.parseColor("#4f3d3d"));
+            distance_vector.update();
+        }
+        else if(mode == 4){
+            // set the fill color (if fill color is not set or is TRANSPARENT, then no fill is drawn)
+            distance4.setFillAlpha(0);
+            distance3.setFillAlpha(0);
+            distance2.setFillColor(Color.parseColor("#6e2424"));
+            distance1.setFillColor(Color.parseColor("#6e2424"));
+            distance_vector.update();
+        }
+        else if(mode == 5){
+            // set the fill color (if fill color is not set or is TRANSPARENT, then no fill is drawn)
+            distance4.setFillAlpha(0);
+            distance3.setFillAlpha(0);
+            distance2.setFillAlpha(0);
+            distance1.setFillColor(Color.parseColor("#961515"));
+            distance_vector.update();
+        }
+
+
+        else if(mode == 6){
+            // set the fill color (if fill color is not set or is TRANSPARENT, then no fill is drawn)
+            distance4.setFillColor(Color.parseColor("#555555"));
+            distance3.setFillColor(Color.parseColor("#555555"));
+            distance2.setFillColor(Color.parseColor("#555555"));
+            distance1.setFillColor(Color.parseColor("#555555"));
+            distance4.setFillAlpha(1);
+            distance3.setFillAlpha(1);
+            distance2.setFillAlpha(1);
+            distance_vector.update();
+            mode = 0;
         }
     }
 }
